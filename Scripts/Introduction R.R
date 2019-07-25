29dbd87a8e318290e1a116c1c5bdf911921f8521
Denise Treacher <denise.treacher@mq.edu.au>
2019-07-25 15:37:40 +1000

    save areas and complete excercise

--- Scripts/Introduction R.R
+++ Scripts/Introduction R.R
@@ -4,3 +4,15 @@ library(tidyverse)
 
 print("hello, world")
 
+area_hectares <-1 # land area in hectares 
+
+area_hectares
+
+1
+2.47 * area_hectares # convert to acres
+area_acres
+
+length <- 1 # length in metres
+width <- 10 # width in metres
+area <- length * width
+area
