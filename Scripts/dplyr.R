d87e0eea414578c955078a09e14dab5ff3d28894
Denise Treacher <denise.treacher@mq.edu.au>
2019-07-26 14:18:34 +1000

    Excersise Counting mutating

--- Scripts/dplyr.R
+++ Scripts/dplyr.R
@@ -38,3 +38,53 @@ interviews_total_meals <- interviews %>%
 view(interviews_total_meals)
 interviews_total_meals
 
+interviews %>%
+  group_by(village) %>%
+  summarize(mean_no_membrs = mean(no_membrs))
+
+interviews %>%
+  group_by(village, memb_assoc) %>%
+  summarize(mean_no_membrs = mean(no_membrs))
+
+# eliminate missing data or NA's
+interviews %>%
+  filter(!is.na(memb_assoc)) %>%
+  group_by(village, memb_assoc) %>%
+  summarize(mean_no_membrs = mean(no_membrs))
+
+interviews %>%
+  filter(!is.na(memb_assoc)) %>%
+  group_by(village, memb_assoc) %>%
+  summarize(mean_no_membrs = mean(no_membrs),
+    min_membrs = min(no_membrs))
+
+#arrange in smallest to largest
+
+interviews %>%
+  filter(!is.na(memb_assoc)) %>%
+  group_by(village, memb_assoc) %>%
+  summarize(mean_no_membrs = mean(no_membrs),
+            min_membrs = min(no_membrs)) %>%
+arrange(min_membrs)
+
+# change to larges to smallest
+interviews %>%
+  filter(!is.na(memb_assoc)) %>%
+  group_by(village, memb_assoc) %>%
+  summarize(mean_no_membrs = mean(no_membrs),
+            min_membrs = min(no_membrs)) %>%
+  arrange(desc(min_membrs))
+
+# counting
+
+interviews %>% count(village) # this counts but doesn't srt by size 
+  
+interviews %>% count(village, sort=TRUE) # counts largest to smallest
+
+# excersise
+interviews %>%
+  filter(!is.na(memb_assoc)) %>%
+  group_by(village, memb_assoc) %>%
+  summarize(mean_no_membrs = mean(no_membrs),
+            min_membrs = min(no_membrs), max_membrs = max(no_membrs),
+n = n())
