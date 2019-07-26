2d13064100e7d65aab0c025e6fe70933579f51bc
Denise Treacher <denise.treacher@mq.edu.au>
2019-07-26 11:35:20 +1000

    Creating Factors

--- Scripts/Starting with Data.R
+++ Scripts/Starting with Data.R
@@ -54,3 +54,50 @@ tail(as.data.frame(interviews_100))
 inteviews_last <- tail(as.data.frame(interviews_100))
 nrow(inteviews_last)                   
 interviews_last <- tail(interviews, 1)
+
+
+# Factors
+
+floor_types <- factor(c("earth", "cement", "cement","earth"))
+levels(floor_types)
+nlevels(floor_types)       
+
+## Reordering
+floor_types <- factor(c("earth", "cement", "cement","earth"), levels=c("earth","cement"))
+
+levels(floor_types)[2] <- "brick"
+levels(floor_types)
+floor_types
+
+as.character(floor_types)
+
+year_fct <- factor(c(1990, 1983, 1977, 1998, 1990))
+
+as.numeric(year_fct)
+
+#to avoid this 
+
+as.character(year_fct)
+as.numeric(as.character(year_fct))
+
+levels(year_fct)[year_fct]
+as.numeric(levels(year_fct))[year_fct]
+
+table(year_nmb)
+table(year_fct)
+summary(year_fct)           
+
+
+affect_conflicts <- interviews$affect_conflicts
+
+affect_conflicts <- as.factor(affect_conflicts) 
+
+plot(affect_conflicts)
+
+#to avoid missing data
+affect_conflicts <- interviews$affect_conflicts
+affect_conflicts [is.na(affect_conflicts)] <- "undetermined"
+affect_conflicts <- as.factor(affect_conflicts)
+plot(affect_conflicts)
+
+
